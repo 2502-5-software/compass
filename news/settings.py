7bbd1ea10281443b95ee773e3f3b14d761a8bc5a
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-29 11:49:24 +0300

    add news article api

--- news/settings.py
+++ news/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'newsapp',
     'cloudinary',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -85,22 +86,22 @@ WSGI_APPLICATION = 'news.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config('DATABASE_ENGINE'),
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USER'),
-#         'PASSWORD': config('DATABASE_PASSWORD'),
-#         'HOST': config('DATABASE_HOST'),
-#         'PORT': config('DATABASE_PORT'),
-#     }
-# }
-
 DATABASES = {
-    'default':  dj_database_url.parse(config(
-        'DATABASE_URL'
-        ))
+    'default': {
+        'ENGINE': config('DATABASE_ENGINE'),
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+    }
 }
+
+# DATABASES = {
+#     'default':  dj_database_url.parse(config(
+#         'DATABASE_URL'
+#         ))
+# }
 
 # Cloudinary Configuration
 
---
+++ newsapp/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import NewsArticle, Category, Comment  
+
+
+class NewsArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NewsArticle
+        fields = '__all__'
