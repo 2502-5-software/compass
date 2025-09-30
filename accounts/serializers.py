f7646ddb1e19e0232ef1c9f6376319043202595e
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-09-30 11:05:34 +0300

    add login serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import CustomUser, Profile
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
     
     class Meta:
+        
         model = CustomUser
         fields = ['id', 'email', 'username', 'password', 'first_name', 'last_name']
         
@@ -24,3 +26,20 @@ class RoleUpdateSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['role']
         
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+    
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+        user = authenticate(email=email, password=password)
+        
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        
+        if not user.is_active:
+            raise serializers.ValidationError("User account is disabled")
+        
+        return user
